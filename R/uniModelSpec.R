c6c19a40b9f4bdc874194392dd227348203c5ee2
Michael-Xiu <997611764@qq.com>
2023-03-22 14:43:58 +0800

    initialize uniModelSpec

---
+++ R/uniModelSpec.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param fit 
+#' @param init.pars 
+#' @param fixed.pars 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+uniModelSpec <- function(fit, init.pars = NULL, fixed.pars = NULL) {
+  print("hello world")
+}
