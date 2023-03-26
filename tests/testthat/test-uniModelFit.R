a3167f9d47c4e2dc1c87a6ac1da7d6aa771f2e1b
yyf201108 <yyf201108@gmail.com>
2023-03-26 19:58:48 +0800

    uniModelFit to be modified.

--- tests/testthat/test-uniModelFit.R
+++
@@ -1,6 +0,0 @@
-test_that("uniModelFit params conincide with precomputed model", {
-  data("data_log_volume")
-  model_spec <- uniModelSpec(fit = TRUE)
-  model_par <- uniModelFit(data_log_volume, model_spec)$par
-  expect_equal(model_par, load("./tests/testthat/EM_result.rda"))
-})
