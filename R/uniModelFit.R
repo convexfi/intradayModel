9d45b960cd510cecd7d30ced973bd294ff1bc184
yyf201108 <yyf201108@gmail.com>
2023-03-26 15:08:52 +0800

    Modify the MARSS model param specification. Add warning of dimension of phi. Modify the extract_value() function.

--- R/uniModelFit.R
+++ R/uniModelFit.R
@@ -1,24 +1,30 @@
 #' Title
 #'
-#' @param data Volume data matrix of size n_bin * n_day with no NA for fitting
+#' @param data.train log volume data matrix of size n_bin * n_day with no NA for fitting
 #' @param modelSpec modelSpec object from function uniModelSpec
 #' @param control List of control variables, e.g., maxit, reltol
 #'
 #' @return
+#' @importFrom magrittr %>%
 #' @export
 #'
 #' @examples
-uniModelFit <- function(data, modelSpec, control = list(maxit = 1000)) {
+uniModelFit <- function(data.train, modelSpec, control = list(maxit = 1000, tol)) {
   
   # check if fit is necessary
+  if (!is.list(modelSpec)) stop("tbd.")
   if (sum(modelSpec$fitFlag) == 0) {
     cat("All parameters are fixed. No need to fit.\n")
     break
   }
   
-  data.train <- log(as.matrix(data))
-  n_bin <- nrow(data)
-  n_day <- ncol(data)
+  # error control
+  if (!is.matrix(data.train) && !is.data.frame(data.train)) stop("data.train must be a matrix or data.frame.")
+  if (anyNA(data.train)) stop("data.train must have no NA.")
+  
+  data.train <- as.matrix(data.train)
+  n_bin <- nrow(data.train)
+  n_day <- ncol(data.train)
   n_bin_total <- n_bin * n_day
   
   ## MARSS parameters
@@ -27,47 +33,54 @@ uniModelFit <- function(data, modelSpec, control = list(maxit = 1000)) {
   MARSS_mdoel$init.gen <- list()
   
   ## State Equation
-  Bt = array(list(0), c(2, 2, nBin_train))
-  b1 = matrix(list(1), n_bin)
-  b1[1] = extract_value("a_eta", modelSpec)
-  Bt[1, 1, ] = rep(b1, nDay_train)
-  Bt[2, 2, ] = extract_value("a_mu", modelSpec)
-
-  Qt = array(list(0), c(2, 2, nBin_train))
-  q1 = matrix(list(1e-10), n_bin)
-  q1[1] = extract_value("var_eta", modelSpec)
-  Qt[1, 1, ] = rep(q1, nDay_train)
-  Qt[2, 2, ] = extract_value("var_mu", modelSpec)
+  Bt <- array(list(0), c(2, 2, n_bin_total))
+  b1 <- matrix(list(1), n_bin)
+  b1[1] <- extract_value("a_eta", modelSpec)
+  Bt[1, 1, ] <- rep(b1, n_day)
+  Bt[2, 2, ] <- extract_value("a_mu", modelSpec)
   
-  U = "zero"
+  Qt <- array(list(0), c(2, 2, n_bin_total))
+  q1 <- matrix(list(1e-10), n_bin)
+  q1[1] <- extract_value("var_eta", modelSpec)
+  Qt[1, 1, ] <- rep(q1, n_day)
+  Qt[2, 2, ] <- extract_value("var_mu", modelSpec)
+  
+  U <- "zero"
   
   ## Measurement Equation
-  Z = array(list(1, 1), c(1, 2))
+  Z <- array(list(1, 1), c(1, 2))
   
-  At = array(list(0), dim = c(1,1,nBin_train))
-  a_vec = rep(0, n_bin)
-  for (n in 1:n_bin) {
-    a_vec[n] <- paste("a", n, sep = "")
+  At = array(list(0), dim = c(1,1,n_bin_total))
+  a_vec = extract_value("phi", modelSpec)
+  if (a_vec == "phi" || length(a_vec) != nbin){
+    if (length(a_vec) != nbin) warning("Dimensions of input data and pre-fixed phi aren't compatible.\n
+                                       The values of fixed phi are ignored.")
+    for (n in 1:n_bin) {
+      a_vec[n] <- paste("a", n, sep = "")
+    }
+    
   }
-  At[1, 1, ] = rep(a_vec, nDay_train)
+  At[1, 1, ] = rep(a_vec, n_day)
   
-  R = matrix(list('r1'), 1, 1)
+  R <- extract_value("r1", modelSpec) %>%
+    list() %>%
+    matrix(1,1)
   
   ## Initial State
-  x0 = matrix(list("x01", "x02"), 2, 1)
-  V0 = "unconstrained"
+  x0 <- extract_value("r1", modelSpec) %>%
+    matrix(2, 1)
+  V0 <- extract_value("V0", modelSpec)
 }
 
 extract_value <- function(name, modelSpec) {
   len <- length(modelSpec$par[[name]])
   if (modelSpec$fitFlag[[name]]) {
-    a_vec <- rep(NA, len)
-    for (n in 1:n_bin) {
-      a_vec[n] <- paste(name, ".", n, sep = "")
-    }
+    name <- switch(name,
+                   "x0"= list("x01","x02"),
+                   "V0" = "unconstrained",
+                   name)
     return(name)
   } else {
     return(modelSpec$par[[name]])
   }
 }
-
