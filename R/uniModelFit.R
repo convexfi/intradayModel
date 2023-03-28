7fb04d345e09c271e3b2c32f245159a5e1aa3eb2
yyf201108 <yyf201108@gmail.com>
2023-03-28 19:39:49 +0800

    Modify uniModelFit() and EM_param().

--- R/uniModelFit.R
+++ R/uniModelFit.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-uniModelFit <- function(data.train, modelSpec, control = list(maxit = 3000, abstol = 1e-4)) {
+uniModelFit <- function(data, modelSpec, control = list(maxit = 3000, abstol = 1e-4)) {
   
   # check if fit is necessary
   if (!is.list(modelSpec)) stop("tbd.")
@@ -19,92 +19,40 @@ uniModelFit <- function(data.train, modelSpec, control = list(maxit = 3000, abst
   }
   
   # error control
-  if (!is.matrix(data.train) && !is.data.frame(data.train)) stop("Input data must be a matrix or data.frame.")
-  if (anyNA(data.train)) stop("Input data must have no NA.")
+  if (!is.matrix(data) && !is.data.frame(data)) stop("Input data must be a matrix or data.frame.")
+  if (anyNA(data)) stop("Input data must have no NA.")
   
   
-  data.train <- as.matrix(data.train)
-  n_bin <- nrow(data.train)
-  n_day <- ncol(data.train)
+  data <- as.matrix(data)
+  n_bin <- nrow(data)
+  n_day <- ncol(data)
   n_bin_total <- n_bin * n_day
   
   ## reform data
-  data.train_reform <- data.train %>%
-    as.list() %>%
-    unlist()
+  # data.train_reform <- data %>%
+  #                      as.list() %>%
+  #                      unlist()
   
-  ## MARSS parameters
-  MARSS_model <- list()
-  MARSS_model$model.gen <- list()
-  MARSS_model$init.gen <- list()
+  args <- list(data = data, n_bin = n_bin,
+               n_day = n_day, n_bin_total = n_bin_total,
+               modelSpec = modelSpec,
+               control = control)
   
-  ## State Equation
-  Bt <- array(list(0), c(2, 2, n_bin_total))
-  b1 <- matrix(list(1), n_bin)
-  b1[1] <- extract_value("a_eta", modelSpec)
-  Bt[1, 1, ] <- rep(b1, n_day)
-  Bt[2, 2, ] <- extract_value("a_mu", modelSpec)
   
-  Qt <- array(list(0), c(2, 2, n_bin_total))
-  q1 <- matrix(list(1e-10), n_bin)
-  q1[1] <- extract_value("var_eta", modelSpec)
-  Qt[1, 1, ] <- rep(q1, n_day)
-  Qt[2, 2, ] <- extract_value("var_mu", modelSpec)
+  result <- do.call(MARSS_spec, args = args)
+  kalman <- result$kalman
+  At <- result$At
   
-  U <- "zero"
+  kalman$par <- kalman$start
+  args <- append(args, list(kalman = kalman, At = At))
   
-  ## Measurement Equation
-  Z <- array(list(1, 1), c(1, 2))
+  EM_result <- do.call(EM_param, args = args)
   
-  At = array(list(0), dim = c(1, 1, n_bin_total))
-  a_vec = extract_value("phi", modelSpec)
-  if (a_vec == "phi" || length(a_vec) != n_bin){
-    if (a_vec != "phi" && length(a_vec) != n_bin) warning("Dimensions of input data and pre-fixed phi aren't compatible.\n
-                                       The values of fixed phi are ignored.")
-    for (n in 1:n_bin) {
-      a_vec[n] <- paste("phi", n, sep = "")
-    }
-    
-  }
-  
-  At[1, 1, ] = rep(a_vec, n_day)
-  
-  R <- extract_value("r", modelSpec) %>%
-    list() %>%
-    matrix(1,1)
-  
-  ## Initial State
-  x0 <- extract_value("x0", modelSpec) %>%
-    matrix(2, 1)
-  V0 <- extract_value("V0", modelSpec)
-  
-  ## predefined init value
-  init.default <- list("x0" = matrix(c(10, 0), 2, 1),
-                       "a_eta" = 1, "a_mu" = 0.7,
-                       "r" = 0.08,
-                       "var_eta" = 0.07, "var_mu" = 0.06,
-                       "V0" = matrix(c(1e-10, 0, 1e-10), 3, 1),
-                       "phi" = rowMeans(matrix(data.train_reform, nrow = n_bin)) - mean(data.train_reform)
-  )
-  ## Init param
-  MARSS_model$init.gen <- extract_init(init.default, modelSpec$init, modelSpec$fitFlag)
-  
-  ## EM
-  MARSS_model$model.gen <- list(Z=Z,R=R,A=At,B=Bt, Q=Qt, U=U, x0=x0,V0=V0, tinitx=1)
-  kalman.ours <- MARSS::MARSS(data.train_reform, model=MARSS_model$model.gen, inits = MARSS_model$init.gen, fit=FALSE)
-  kalman.ours$par <- kalman.ours$start
-  
-  # args <- list(y_train = data_log_volume,
-  #              n_bin = n_bin,
-  #              n_bin_total = n_bin_total,
-  #              n_day = n_day,
-  #              control = control
-  #              )
-  EM_result <- EM_param(kalman.ours, modelSpec,
-                        data.train_reform, n_bin,
-                        n_bin_total,
-                        n_day,At,
-                        control)
+  # EM_result <- EM_param(kalman, modelSpec,
+  #                         data.train_reform, n_bin,
+  #                         n_bin_total,
+  #                         n_day,At,
+  #                         control)
   
   modelSpec$par <- trans_MARSStoIntra(EM_result$model$par, modelSpec$par)
   if (EM_result$convergence) {
@@ -116,69 +64,26 @@ uniModelFit <- function(data.train, modelSpec, control = list(maxit = 3000, abst
   
 }
 
-extract_value <- function(name, modelSpec) {
-  if (modelSpec$fitFlag[[name]]) {
-    name <- switch(name,
-                   "x0"= list("x01","x02"),
-                   "V0" = "unconstrained",
-                   name)
-    return(name)
-  } else {
-    return(modelSpec$par[[name]])
-  }
-}
 
-extract_init <- function(init.default, init.pars, fitFlag){
-  all.pars.name <- c("a_eta", "a_mu", "var_eta", "var_mu", "r", "phi", "x0", "V0")
-  for (name in all.pars.name){
-    if (!fitFlag[[name]]) {
-      init.default[[name]] <- NULL
-    }
-    if (name %in% names(init.pars)){
-      init.default[[name]] <- init.pars[[name]]
-    }
-  }
-  init.marss <- list()
-  var.init <- c()
-  a.init <- c()
-  for (name in names(init.default)){
-    tmp <- switch(name,
-                  "phi" = {init.marss$A <- init.default$phi},
-                  "V0" = {init.marss$V0 <- init.default$V0},
-                  "x0" = {init.marss$x0 <- init.default$x0},
-                  "r" = {init.marss$R <- init.default$r},
-                  "a_eta" = {a.init <- append(a.init, init.default$a_eta)},
-                  "a_mu" = {a.init <- append(a.init, init.default$a_mu)},
-                  "var_eta" = {var.init <- append(var.init, init.default$var_eta)},
-                  "var_mu" = {var.init <- append(var.init, init.default$var_mu)}
-    )
-  }
-  if (length(a.init) > 0){
-    init.marss$B <- matrix(a.init, length(a.init), 1)
-  }
-  if (length(var.init) > 0){
-    init.marss$Q <- matrix(var.init, length(var.init), 1)
-  }
-  return (init.marss)
-}
-
-trans_marss2our <- function(marss_model, intra_model){
-  fix <- modelSpec$fitFlag
-  unfixed.names <- names(fix[fix == TRUE])
-  for (name in unfixed.names){
-    modelSpec$par[[name]] <- marss_model
-  }
-}
-
-EM_param <- function(kalman.ours,modelSpec,
-                     y_train, n_bin,
-                     n_bin_total,
-                     n_day,At,
-                     control){
+EM_param <- function(...){
+  args <- list(...)
+  data <- args$data
+  n_bin <- args$n_bin
+  n_bin_total <- args$n_bin_total
+  n_day <- args$n_day
+  modelSpec <- args$modelSpec
+  kalman <- args$kalman
+  control <- args$control
+  At <- args$At
+  
+  data.reform <- data %>%
+    as.list() %>%
+    unlist()
+  
   convergence <- FALSE
-  Z.matrix <- matrix(kalman.ours[["model"]][["fixed"]][["Z"]][,,1], nrow = 1) # matrix(unlist(Z), nrow = 1)
+  Z.matrix <- matrix(kalman[["model"]][["fixed"]][["Z"]][,,1], nrow = 1) # matrix(unlist(Z), nrow = 1)
   jump_interval <- seq(n_bin + 1, n_bin_total, n_bin)
-  y.daily.matrix <- matrix(y_train, n_bin)
+  y.daily.matrix <- matrix(data.reform, n_bin)
   
   
   maxit <- control$maxit
@@ -190,9 +95,8 @@ EM_param <- function(kalman.ours,modelSpec,
   
   for (i in 1: maxit) {
     # Kalman filter & smoother
-    Kf <- MARSS::MARSSkfas(kalman.ours)
-    curr_par <- kalman.ours$par
-    
+    Kf <- MARSS::MARSSkfas(kalman)
+    curr_par <- kalman$par
     
     # update parameter estimation
     Pt <- Ptt1 <- array(NA, c(2, 2, n_bin_total))
@@ -206,10 +110,10 @@ EM_param <- function(kalman.ours,modelSpec,
     update_r <- function(){
       if("phi" %in% unfixed.names) {phi.matrix <- rep(matrix(curr_par$A, nrow = 1), n_day) }
       else {phi.matrix <-unlist(At)} # need input
-      curr_par$R <<- mean(y_train^2 + apply(Pt, 3, function(p) Z.matrix %*% p %*% t(Z.matrix)) -
-                            2 * y_train * as.numeric(Z.matrix %*% Kf$xtT) +
+      curr_par$R <<- mean(data.reform^2 + apply(Pt, 3, function(p) Z.matrix %*% p %*% t(Z.matrix)) -
+                            2 * data.reform * as.numeric(Z.matrix %*% Kf$xtT) +
                             phi.matrix^2 -
-                            2 * y_train * phi.matrix +
+                            2 * data.reform * phi.matrix +
                             2 * phi.matrix * as.numeric(Z.matrix %*% Kf$xtT))
     }
     update_var_eta <- function(){
@@ -248,7 +152,7 @@ EM_param <- function(kalman.ours,modelSpec,
     }
     
     # stopping criteria
-    diff <- norm(as.numeric(unlist(kalman.ours$par)) -
+    diff <- norm(as.numeric(unlist(kalman$par)) -
                    as.numeric(unlist(curr_par)), type = "2")
     if (diff < abstol) {
       convergence <- TRUE
@@ -258,11 +162,24 @@ EM_param <- function(kalman.ours,modelSpec,
     if (i %% 25 == 0) {
       cat("iter:", i, " diff:", diff, "\n", sep = "")
     }
-    kalman.ours$par <- curr_par
+    kalman$par <- curr_par
+    
   }
+  # reshape phi
+  phi_names <- c()
+  for (i in 1:n_bin){
+    phi_names <- append(phi_names, paste(paste("phi", i, sep = "")))
+  }
+  kalman$par$A <- array(kalman$par$A, dim = c(n_bin,1), dimnames = list(phi_names,NULL))
   if (!convergence) warning("No convergence")
-  result <- list("model" = kalman.ours, "convergence" = convergence)
+  result <- list("model" = kalman, "convergence" = convergence)
   return (result)
 }
 
-
+fetch_par_log <- function(par_log, index) {
+  par_list <- list()
+  for (i in 1:length(par_log)) {
+    par_list <- list.append(par_list, par_log[[i]][[index]])
+  }
+  return(do.call(cbind, par_list))
+}
