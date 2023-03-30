1a068d1c37564d5c9a456a86ab07cc4ebbcb9d65
yyf201108 <yyf201108@gmail.com>
2023-03-30 21:32:10 +0800

    Add test for uniModelfit with partial fixed params. Modify the precomputed params data.

--- tests/testthat/test-uniModelFit.R
+++ tests/testthat/test-uniModelFit.R
@@ -1,7 +1,7 @@
 test_that("uniModelFit params conincide with precomputed model", {
   data("data_log_volume")
-  load("./tests/testthat/EM_result.rda")
-  precomputed_par <- EM_result
+  load("./tests/testthat/params.rda")
+  precomputed_par <- params
   
   modelSpec <- uniModelSpec(fit = TRUE)
   
@@ -29,4 +29,42 @@ test_that("uniModelFit params conincide with precomputed model", {
   
   
   expect_equal(EM_result_test$model$par, precomputed_par, tolerance = 1e-3)
+})
+
+test_that("uniModelFit with partial fixed params conincide with precomputed model", {
+  data("data_log_volume")
+  load("./tests/testthat/params.rda")
+  precomputed_par <- params
+  
+  data <- data_log_volume
+  data <- as.matrix(data)
+  n_bin <- nrow(data)
+  n_day <- ncol(data)
+  n_bin_total <- n_bin * n_day
+  
+  fixed_phi <- precomputed_par$A
+  
+  fixed.pars <- list("x0" = matrix(list(10, 0), 2, 1), "phi" = fixed_phi)
+  modelSpec <- uniModelSpec(fit = TRUE, fixed.pars = fixed.pars)
+  
+  
+  control = list(maxit = 3000, abstol = 1e-4, log.switch = TRUE)
+  
+  args <- list(data = data, n_bin = n_bin,
+               n_day = n_day, n_bin_total = n_bin_total,
+               modelSpec = modelSpec,
+               control = control)
+  
+  
+  result <- do.call(MARSS_spec, args = args)
+  kalman <- result$kalman
+  At <- result$At
+  
+  kalman$par <- kalman$start
+  args <- append(args, list(kalman = kalman, At = At))
+  
+  EM_result_test <- do.call(EM_param, args = args)
+  
+  
+  expect_equal(EM_result_test$model$par[c("R","B","Q","V0")], precomputed_par[c("R","B","Q","V0")], tolerance = 1e-1)
 })
