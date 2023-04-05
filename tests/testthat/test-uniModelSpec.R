ccc460866688b129b138edf2909afda391642d80
Michael-Xiu <997611764@qq.com>
2023-04-05 14:49:16 +0800

    correct the expected output of test 2 on uniModelSpec

--- tests/testthat/test-uniModelSpec.R
+++ tests/testthat/test-uniModelSpec.R
@@ -49,7 +49,7 @@ test_that("unimodel specification return coincide with the predefined ones versi
   #                                "var_eta" = NA, "var_mu" = NA,
   #                                "r" = NA, "phi" = NA,
   #                                "x0" = matrix(0, 2), "V0" = matrix(NA,3))
-  predefinde_model$init <- list("a_eta" =  1,"x0" = matrix(0, 2))
+  predefinde_model$init <- list("a_eta" =  1)
   predefinde_model$fitFlag <- list("a_eta" =  TRUE, "a_mu" = TRUE,
                                    "var_eta" = FALSE, "var_mu" = TRUE,
                                    "r" = TRUE, "phi" = TRUE,
