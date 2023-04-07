a3af87de78f302c315ab9489d44131ca4b4461db
Michael-Xiu <997611764@qq.com>
2023-04-07 18:31:43 +0800

    format uniModelSpec with R tool

--- R/uniModelSpec.R
+++ R/uniModelSpec.R
@@ -10,11 +10,11 @@
 #' @examples
 uniModelSpec <- function(fit = FALSE, init.pars = NULL, fixed.pars = NULL) {
   modelSpec <- list()
-  
+
   # error control
   if (!is.null(init.pars) && !is.list(init.pars)) stop("init.pars must be a list.")
   if (!is.null(fixed.pars) && !is.list(fixed.pars)) stop("fixed.pars must be a list.")
-  
+
   # modelSpec class properties
   all_pars_name <- c("a_eta", "a_mu", "var_eta", "var_mu", "r", "phi", "x0", "V0")
   modelSpec$par$"a_eta" <- NA
@@ -26,19 +26,18 @@ uniModelSpec <- function(fit = FALSE, init.pars = NULL, fixed.pars = NULL) {
   modelSpec$par$"x0" <- matrix(NA, 2)
   modelSpec$par$"V0" <- matrix(NA, 3)
   modelSpec$init <- list()
-  
+
   # read in input parameters
   fixed.pars <- cleanParsList(fixed.pars)
   init.pars <- cleanParsList(init.pars)
   for (name in all_pars_name) {
     if (name %in% names(fixed.pars)) {
       modelSpec$par[[name]] <- fixed.pars[[name]]
-    }
-    else if (name %in% names(init.pars)){
+    } else if (name %in% names(init.pars)) {
       modelSpec$init[[name]] <- init.pars[[name]]
     }
   }
-  
+
   # decide if each variable requires fitting
   if (fit == FALSE) {
     if (anyNA(unlist(modelSpec$par))) {
@@ -54,9 +53,9 @@ uniModelSpec <- function(fit = FALSE, init.pars = NULL, fixed.pars = NULL) {
       modelSpec$fit_request[[name]] <- FALSE
     }
   }
-  
-  # unify the modelSpec parameters format 
+
+  # unify the modelSpec parameters format
   modelSpec <- unifyModelFormat(modelSpec)
-  
+
   return(modelSpec)
 }
