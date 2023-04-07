4a0f5b2e2f524f528e1462b2b9f8009734bbab72
Michael-Xiu <997611764@qq.com>
2023-04-07 20:45:52 +0800

    autoformat uniModelPred

--- R/uniModelPred.R
+++ R/uniModelPred.R
@@ -13,32 +13,32 @@ uniModelPred <- function(data, uniModel, out.sample) {
   if (!is.matrix(data) && !is.data.frame(data)) stop("data must be a matrix or data.frame.")
   if (anyNA(data)) stop("data must have no NA.")
   # isIntraModel(uniModel, data)
-  
+
   # check if fit is necessary
   if (Reduce("+", uniModel$fit_request) != 0) {
     stop("All parameters must be fixed.\n")
   }
-  
+
   # one-step ahead prediction using MARSS
   y.pred <- marss_predict(data, uniModel)
   y.pred.out.sample <- tail(y.pred, nrow(data) * out.sample)
-  
-  return (y.pred.out.sample)
+
+  return(y.pred.out.sample)
 }
 
-marss_predict <- function(data, uniModel){
+marss_predict <- function(data, uniModel) {
   data <- as.matrix(data)
   args <- list(
     data = data,
     uniModel = uniModel
   )
-  
+
   marss_obj <- do.call(specify_marss, args = args)
   Kf <- MARSS::MARSSkfas(marss_obj)
   x_pred <- Kf[["xtt1"]]
-  seasonal <- uniModel$par$phi[,1]
+  seasonal <- uniModel$par$phi[, 1]
   names(seasonal) <- NULL
-  y.pred <- x_pred[1,] + x_pred[2,] + rep(seasonal, ncol(data))
-  
-  return (y.pred)
+  y.pred <- x_pred[1, ] + x_pred[2, ] + rep(seasonal, ncol(data))
+
+  return(y.pred)
 }
