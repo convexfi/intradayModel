dda2c5dfcfc9addff0744cc6b357c32e2ff5e0c8
yyf201108 <yyf201108@gmail.com>
2023-04-13 16:29:49 +0800

    Add doc for data.

---
+++ R/AAPL_volume.R
@@ -0,0 +1,14 @@
+#' 15-min intraday volume of AAPL from 2019-01-02 to 2019-06-28
+#'
+#'
+#' @docType data
+#'
+#' @usage data(AAPL_volume)
+#'
+#' @format 26 * 124 matrix dataset containing stock AAPL's data of 26 bins wthin 124 days.
+#'
+#' @source tbd
+#'
+#' @keywords dataset
+#'
+"AAPL_volume"
