72ad14203acb97470efc6c2772ecf5034c960b36
Michael-Xiu <997611764@qq.com>
2023-04-21 13:54:44 +0800

    allow direct plotting out

--- R/plotting.R
+++ R/plotting.R
@@ -1,10 +1,10 @@
-#' @import patchwork 
+#' @import patchwork
 #' @import ggplot2
 #' @importFrom magrittr %>%
 plot_components <- function(smooth_forecast_result) {
-  i <- original <- daily <-  seasonal <- dynamic <- NULL
+  i <- original <- daily <- seasonal <- dynamic <- NULL
   components <- smooth_forecast_result[["components"]]
-  
+
   plt_data <-
     data.frame(
       original = smooth_forecast_result$real.signal,
@@ -13,12 +13,12 @@ plot_components <- function(smooth_forecast_result) {
       dynamic = components[[grep("dynamic", names(components))]]
     )
   plt_data_log <- log(plt_data)
-  plt_data_log$i <- plt_data$i <- c(1: nrow(plt_data))
-  
-  text_size = 10
+  plt_data_log$i <- plt_data$i <- c(1:nrow(plt_data))
+
+  text_size <- 10
   p1 <- plt_data_log %>%
     ggplot() +
-    geom_line(aes(x = i, y= original), alpha = 0.8, color = "steelblue", size = 0.4) +
+    geom_line(aes(x = i, y = original), alpha = 0.8, color = "steelblue", size = 0.4) +
     xlab(expression(tau)) +
     ylab("Original") +
     theme_bw() +
@@ -31,14 +31,14 @@ plot_components <- function(smooth_forecast_result) {
       legend.text = element_text(size = text_size, face = "bold"),
       # legend.title = element_blank(),
       legend.key.size = unit(1, "cm"),
-      plot.title = element_text(size=18, face = "bold", hjust = 0.5),
-      axis.title.x=element_blank(),
-      axis.text.x=element_blank()
+      plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank()
     )
-  
+
   p2 <- plt_data_log %>%
     ggplot() +
-    geom_line(aes(x = i, y= daily), alpha = 0.8, color = "steelblue", size = 0.6) +
+    geom_line(aes(x = i, y = daily), alpha = 0.8, color = "steelblue", size = 0.6) +
     xlab(expression(tau)) +
     ylab("Daily") +
     theme_bw() +
@@ -51,14 +51,14 @@ plot_components <- function(smooth_forecast_result) {
       legend.text = element_text(size = text_size, face = "bold"),
       # legend.title = element_blank(),
       legend.key.size = unit(1, "cm"),
-      plot.title = element_text(size=18, face = "bold", hjust = 0.5),
-      axis.title.x=element_blank(),
-      axis.text.x=element_blank()
+      plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank()
     )
-  
+
   p3 <- plt_data_log %>%
     ggplot() +
-    geom_line(aes(x = i, y= seasonal), alpha = 0.8, color = "steelblue", size = 0.4) +
+    geom_line(aes(x = i, y = seasonal), alpha = 0.8, color = "steelblue", size = 0.4) +
     xlab(expression(tau)) +
     ylab("Seasonal") +
     theme_bw() +
@@ -71,14 +71,14 @@ plot_components <- function(smooth_forecast_result) {
       legend.text = element_text(size = text_size, face = "bold"),
       # legend.title = element_blank(),
       legend.key.size = unit(1, "cm"),
-      plot.title = element_text(size=18, face = "bold", hjust = 0.5),
-      axis.title.x=element_blank(),
-      axis.text.x=element_blank()
+      plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank()
     )
-  
+
   p4 <- plt_data_log %>%
     ggplot() +
-    geom_line(aes(x = i, y= dynamic), alpha = 0.8, color = "steelblue", size = 0.4) +
+    geom_line(aes(x = i, y = dynamic), alpha = 0.8, color = "steelblue", size = 0.4) +
     xlab(expression(tau)) +
     ylab("Intraday\nDynamic") +
     theme_bw() +
@@ -93,14 +93,14 @@ plot_components <- function(smooth_forecast_result) {
       legend.text = element_text(size = text_size, face = "bold"),
       # legend.title = element_blank(),
       legend.key.size = unit(1, "cm"),
-      plot.title = element_text(size=18, face = "bold", hjust = 0.5)
+      plot.title = element_text(size = 18, face = "bold", hjust = 0.5)
     )
-  
-  fig <- p1/p2/p3/p4 + 
-    plot_annotation(title = "Decomposition of intraday signal (log scale)",
-                    theme =  theme(plot.title = element_text(size=16, face = "bold", hjust = 0.5)))
-                    
-  return(fig)
+
+  p1 / p2 / p3 / p4 +
+    plot_annotation(
+      title = "Decomposition of intraday signal (log scale)",
+      theme = theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
+    )
 }
 
 #' @import ggplot2
@@ -108,7 +108,7 @@ plot_components <- function(smooth_forecast_result) {
 plot_performance <- function(smooth_forecast_result) {
   # i <- real <- pred <- NULL
   # signal <- type <- NULL
-  
+
   # determine type
   if (sum(grepl("smooth", names(smooth_forecast_result)))) {
     type <- "smooth"
@@ -117,23 +117,23 @@ plot_performance <- function(smooth_forecast_result) {
     type <- "forecast"
     title <- "One-bin-ahead prediction (log scale)"
   }
-  
+
   plt_data <-
     data.frame(
       real = smooth_forecast_result$real.signal,
       output = smooth_forecast_result[[grep(type, names(smooth_forecast_result))]]
     )
   plt_data_log <- log(plt_data)
-  plt_data_log$i <- plt_data$i <- c(1: nrow(plt_data))
+  plt_data_log$i <- plt_data$i <- c(1:nrow(plt_data))
 
 
   plt_reshape <- plt_data_log %>%
     reshape2::melt(
       id.vars = c("i"),
       variable.name = "variable", value.name = "value"
-    ) 
+    )
 
-  text_size = 14
+  text_size <- 14
   plt_reshape %>%
     ggplot() +
     geom_line(aes(x = i, y = value, color = variable), alpha = 0.8, size = 0.4) +
@@ -143,62 +143,16 @@ plot_performance <- function(smooth_forecast_result) {
     theme_bw() +
     theme(
       axis.title = element_text(size = text_size, face = "bold"),
-      legend.position =  c(.8, .9),
+      legend.position = c(.8, .9),
       legend.justification = c(0, 1),
       legend.text = element_text(size = text_size, face = "bold"),
       legend.title = element_blank(),
       legend.key.size = unit(1, "cm"),
-      plot.title = element_text(size=18, face = "bold", hjust = 0.5),
-      axis.text.x=element_blank()
-    ) + 
-    plot_annotation(title = title,
-                    theme =  theme(plot.title = element_text(size=16, face = "bold", hjust = 0.5)))
+      plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+      axis.text.x = element_blank()
+    ) +
+    plot_annotation(
+      title = title,
+      theme = theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5))
+    )
 }
-
-
-# uniModelPlot <- function(data, filter.result, type){
-#   data.reform <- data %>%
-#     as.list() %>%
-#     unlist()
-#   plt_data_log <-
-#     data.frame(
-#       volume = as.vector(data.reform),
-#       daily = filter.result[["daily"]],
-#       seasonal = filter.result[["seasonal"]],
-#       dynamic = filter.result[["dynamic"]],
-#       i = 1:(nrow(data_log_volume) * ncol(data_log_volume))
-#     )
-#   
-#   text_size = 10
-#   y.value <- y.lab <- NULL
-#   switch (type,
-#           "volume" = {y.value = quote(volume)
-#           y.lab = "Intraday\nVolume"},
-#           "daily" = {y.value = quote(daily)
-#           y.lab = "Daily"},
-#           "seasonal" = {y.value = quote(seasonal)
-#           y.lab = "seasonal"},
-#           "dynamic" = {y.value = quote(dynamic)
-#           y.lab = "Intraday\nDynamic"}
-#   )
-#   p1 <- plt_data_log %>%
-#     ggplot() +
-#     geom_line(aes(x = i, y= !!y.value), alpha = 0.8, color = "steelblue", size = 0.4) +
-#     xlab(expression(tau)) +
-#     ylab(y.lab) +
-#     theme_bw() +
-#     theme(
-#       axis.title = element_text(size = text_size, face = "bold"),
-#       legend.position = "right",
-#       legend.justification = c(0, 1),
-#       legend.box.just = "left",
-#       legend.margin = margin(8, 8, 8, 8),
-#       legend.text = element_text(size = text_size, face = "bold"),
-#       # legend.title = element_blank(),
-#       legend.key.size = unit(1, "cm"),
-#       plot.title = element_text(size=18, face = "bold", hjust = 0.5),
-#       axis.title.x=element_blank(),
-#       axis.text.x=element_blank()
-#     )
-#   p1
-# }
